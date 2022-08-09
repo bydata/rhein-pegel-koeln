d74946b32ad778eb090a17044dd7fe27956e3089
bydata <ansgar.wolsing@gmail.com>
2022-08-09 20:07:50 +0200

    removed lubridate dependency

--- R/merge-data.R
+++ R/merge-data.R
@@ -4,8 +4,7 @@ files <- list.files(filepath, pattern = "pegel.\\d+.csv")
 combined <- purrr::map_dfr(file.path(filepath, files), read.csv)
 
 result <- list()
-result$date <- lubridate::parse_date_time(
-  combined$Datum, "%d. %B %Y", tz = "Europe/Berlin")
+result$date <- strptime(combined$Datum, "%d. %B %Y", tz = "Europe/Berlin")
 result$time <- combined$Uhrzeit
 result$water_level <- combined$Pegel
 result <- as.data.frame(result)
