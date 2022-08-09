e164bf3725d4d471c03b7e433e6ded263efc5842
bydata <ansgar.wolsing@gmail.com>
2022-08-09 22:15:06 +0200

    solving #1

--- R/merge-data.R
+++ R/merge-data.R
@@ -1,12 +1,26 @@
+library(purrr)
+library(dplyr)
+
 # Merge files into a combine dataset
 filepath <- file.path("data", "fetched-results")
 files <- list.files(filepath, pattern = "pegel.\\d+.csv")
 combined <- purrr::map_dfr(file.path(filepath, files), read.csv)
 
-result <- list()
-result$date <- strptime(combined$Datum, "%d. %B %Y", tz = "Europe/Berlin")
-result$time <- combined$Uhrzeit
-result$water_level <- combined$Pegel
-result <- as.data.frame(result)
+# translate the German month name to English
+de_months <- c(
+  "Januar", "Februar", "März", "April", "Mai", "Juni" , "Juli", "August", 
+  "September", "Oktober", "November",  "Dezember"
+)
+en_months <- month.name
+names(en_months) <- de_months
+
+result <- combined %>% 
+  mutate(month_de = stringr::str_extract(Datum, paste(de_months, collapse = "|")),
+         month_en = en_months[month_de],
+         date_str_en = stringr::str_replace(Datum, month_de, month_en),
+         datetime_str_en = paste(date_str_en, Uhrzeit),
+         date = strptime(date_str_en, "%d. %B %Y", tz = "Europe/Berlin"),
+         datetime = strptime(datetime_str_en, "%d. %B %Y %H:%M", tz = "Europe/Berlin")) %>% 
+  select(date, datetime, water_level = Pegel)
 
 write.csv(result, file.path("data", "pegel-combined.csv"), row.names = FALSE)
