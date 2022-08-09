2211bafd45d8f58cd18694e75df3a9f6b8087e38
bydata <ansgar.wolsing@gmail.com>
2022-08-09 19:08:49 +0200

    recode variables

--- R/merge-data.R
+++ R/merge-data.R
@@ -3,10 +3,11 @@ filepath <- file.path("data", "fetched-results")
 files <- list.files(filepath, pattern = "pegel.\\d+.csv")
 combined <- purrr::map_dfr(file.path(filepath, files), read.csv)
 
-# # format the dataset
-# result <- list()
-# result$date <- lubridate::parse_date_time(
-#   combined$Datum, "%d. %B %Y", tz = "Europe/Berlin")
+result <- list()
+result$date <- lubridate::parse_date_time(
+  combined$Datum, "%d. %B %Y", tz = "Europe/Berlin")
+result$time <- combined$Uhrzeit
+result$water_level <- combined$Pegel
+result <- as.data.frame(result)
 
-
-write.csv(combined, file.path("data", "pegel-combined.csv"), row.names = FALSE)
+write.csv(result, file.path("data", "pegel-combined.csv"), row.names = FALSE)
