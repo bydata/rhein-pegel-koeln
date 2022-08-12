83b5aa76e7e3569041a0aa9aefe0c8a06e64e865
bydata <ansgar.wolsing@gmail.com>
2022-08-12 20:25:18 +0200

    start y at 0.5 m

--- R/plot.R
+++ R/plot.R
@@ -4,11 +4,12 @@ df <- readr::read_csv(file.path("data", "pegel-combined.csv"))
 
 ggplot(df, aes(datetime, water_level)) +
   geom_step(color = "steelblue") +
+  coord_cartesian(ylim = c(0.5, NA)) +
   labs(
-    title = "Wasserpegel des Rheins bei Köln",
+    title = "Pegelstand des Rheins bei Köln",
     caption = "Stadtentwässerungsbetriebe Köln, Stadt Köln",
     x = NULL,
-    y = "Wasserpegel (in m)"
+    y = "Pegelstand (in m)"
   ) +
   theme_minimal() +
   theme(
@@ -24,6 +25,31 @@ ggsave(file.path("plots", "wasserpegel-live.png"), width = 5, height = 4,
        scale = 1.5)
 
 
+ggplot(df, aes(datetime, water_level)) +
+  geom_point(color = "grey50") +
+  geom_smooth(method = "loess", se = TRUE, span = 0.3, color = "steelblue",
+              fill = alpha("#B9DAFF", 0.7)) +
+  coord_cartesian(ylim = c(0.5, NA)) +
+  labs(
+    title = "Pegelstand des Rheins bei Köln",
+    caption = "Stadtentwässerungsbetriebe Köln, Stadt Köln",
+    x = NULL,
+    y = "Pegelstand (in m)"
+  ) +
+  theme_minimal() +
+  theme(
+    plot.background = element_rect(color = NA, fill = "grey98"),
+    text = element_text(color = "grey20"),
+    plot.title = element_text(face = "bold", color = "grey4"),
+    plot.title.position = "plot",
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank()
+  )
+ggsave(file.path("plots", "wasserpegel-live-smoothed.png"), width = 5, height = 4,
+       scale = 1.5)
+
+
+
 
 #' Normalpegel: 3,48 m
 #' https://de.wikipedia.org/wiki/Pegel_K%C3%B6ln#:~:text=Der%20Normalpegel%20liegt%20bei%203%2C48%20Metern.
@@ -36,10 +62,10 @@ ggplot(df, aes(datetime, water_level)) +
            hjust = 0, vjust = 0, label.size = 0, fill = alpha("grey8", 0.2)) +
   coord_cartesian(ylim = c(0, 9)) +
   labs(
-    title = "Wasserpegel des Rheins bei Köln",
+    title = "Pegelstand des Rheins bei Köln",
     caption = "Stadtentwässerungsbetriebe Köln, Stadt Köln",
     x = NULL,
-    y = "Wasserpegel (in m)"
+    y = "Pegelstand (in m)"
   ) +
   theme_minimal() +
   theme(
