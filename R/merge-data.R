94fc83174799e94aa5fe93f036c47632ace3c71f
bydata <ansgar.wolsing@gmail.com>
2022-10-08 15:19:33 +0200

    deleted raw files from repo; changed merging of files so that only the combined file is kept

--- R/merge-data.R
+++ R/merge-data.R
@@ -4,7 +4,9 @@ library(dplyr)
 # Merge files into a combine dataset
 filepath <- file.path("data", "fetched-results")
 files <- list.files(filepath, pattern = "pegel.\\d+.csv")
-combined <- purrr::map_dfr(file.path(filepath, files), read.csv)
+filepath_combined <- file.path("data", "pegel-combined.csv")
+new_data <- purrr::map_dfr(file.path(filepath, files), read.csv)
+combined <- read.csv(filepath_combined)
 
 # translate the German month name to English
 de_months <- c(
@@ -14,14 +16,19 @@ de_months <- c(
 en_months <- month.name
 names(en_months) <- de_months
 
-result <- combined %>% 
+result <- new_data %>% 
   mutate(month_de = stringr::str_extract(Datum, paste(de_months, collapse = "|")),
          month_en = en_months[month_de],
          date_str_en = stringr::str_replace(Datum, month_de, month_en),
          datetime_str_en = paste(date_str_en, Uhrzeit),
-         date = strptime(date_str_en, "%d. %B %Y", tz = "Europe/Berlin"),
-         datetime = strptime(datetime_str_en, "%d. %B %Y %H:%M", tz = "Europe/Berlin")) %>% 
+         date = strptime(date_str_en, "%d. %B %Y", tz = "Europe/Berlin") %>% as.character(),
+         datetime = strptime(datetime_str_en, "%d. %B %Y %H:%M", tz = "Europe/Berlin") %>% as.character()) %>% 
   arrange(datetime) %>% 
   select(date, datetime, water_level = Pegel)
 
-write.csv(result, file.path("data", "pegel-combined.csv"), row.names = FALSE)
+new_combined <- bind_rows(combined, result) %>% 
+  distinct()
+
+# throws an error if file size is not correct and therefore interrupts the Github action
+testthat::expect_equal(nrow(new_combined), nrow(combined) + 1)
+write.csv(new_combined, "foo.csv", row.names = FALSE)
