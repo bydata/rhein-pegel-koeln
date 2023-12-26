8590bd8a82643d5cfa7b9e8d8318ba680094a26c
bydata <ansgar.wolsing@gmail.com>
2023-12-26 13:31:12 +0100

    1m breaks

--- R/plot.R
+++ R/plot.R
@@ -6,7 +6,9 @@ ylim_lower <- 0
 
 ggplot(df, aes(datetime, water_level)) +
   geom_step(color = "steelblue") +
-  scale_y_continuous(labels = scales::number_format(accuracy = 0.01, decimal.mark = ",")) + 
+  scale_y_continuous(
+    breaks = seq(0, 20, 1),
+    labels = scales::number_format(accuracy = 1, decimal.mark = ",")) + 
   coord_cartesian(ylim = c(ylim_lower, NA)) +
   labs(
     title = "Pegelstand des Rheins bei Köln",
@@ -29,10 +31,12 @@ ggsave(file.path("plots", "wasserpegel-live.png"), width = 5, height = 4,
 
 
 ggplot(df, aes(datetime, water_level)) +
-  geom_point(color = "grey50", size = 0.2) +
+  geom_point(color = "grey50", size = 0.05) +
   geom_smooth(method = "loess", se = TRUE, span = 0.1, color = "steelblue",
               fill = alpha("#B9DAFF", 0.7)) +
-  scale_y_continuous(labels = scales::number_format(accuracy = 0.01, decimal.mark = ",")) + 
+  scale_y_continuous(
+    breaks = seq(0, 20, 1),
+    labels = scales::number_format(accuracy = 1, decimal.mark = ",")) + 
   coord_cartesian(ylim = c(ylim_lower, NA)) +
   labs(
     title = "Pegelstand des Rheins bei Köln",
@@ -54,8 +58,6 @@ ggsave(file.path("plots", "wasserpegel-live-smoothed.png"), width = 5, height = 
 
 
 
-
-
 #' Langfristiges Mittel: 2,97 m
 #' https://www.elwis.de/DE/dynamisch/gewaesserkunde/wasserstaende/index.php?target=2&pegelId=a6ee8177-107b-47dd-bcfd-30960ccc6e9c
 
@@ -65,7 +67,9 @@ ggplot(df, aes(datetime, water_level)) +
   annotate("label", label = "Langfristiger Durchschnitt 2,97 m",
            x = min(df$datetime), y = 2.97,
            hjust = 0, vjust = 0, label.size = 0, fill = alpha("grey8", 0.2)) +
-  scale_y_continuous(labels = scales::number_format(accuracy = 0.01, decimal.mark = ",")) + 
+  scale_y_continuous(
+    breaks = seq(0, 20, 1),
+    labels = scales::number_format(accuracy = 1, decimal.mark = ",")) + 
   coord_cartesian(ylim = c(0, 9)) +
   labs(
     title = "Pegelstand des Rheins bei Köln",
